32948e2a32c73d73d112dfb043b17c7ac75192a4
Tony Rizzo <trizzo@koteray.com>
2018-03-23 13:28:18 -0400

    Initial commit

---
+++ Squarer/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Squarer
+//
+//  Created by Tony Rizzo on 11/8/17.
+//  Copyright © 2017 Koteray. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    int value = 5;
+    int squared = value * value;
+    
+    printf("\"%d\" squared is \"%d\"\n",value , squared);
+    
+    return 0;
+}
